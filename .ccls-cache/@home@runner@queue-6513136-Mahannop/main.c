c70edfdd585133792a6b8f38da58da66eb78e90b
6513136-Mahanno <122344487+6513136-Mahannop@users.noreply.github.com>
2023-02-24 07:44:00 +0000

    6513136_queue_ver3

--- .ccls-cache/@home@runner@queue-6513136-Mahannop/main.c
+++ .ccls-cache/@home@runner@queue-6513136-Mahannop/main.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Node.h"
-
-//#include "Queue.h"
+#include "Queue.h"
 
 int main(int argc , char **argv) {
+   /* For Node.h ( only q )
    NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
+   NodePtr tailPtr=NULL; */
+   
+   Queue q; //Most importantly, don't forget to declare struct of queue !
+   q.headPtr = NULL;
+   q.tailPtr = NULL;
+   q.size = 0; 
 /* For struct Queue
-  Queue  q;
+  Queue q;
    q.headPtr=NULL;
    q.tailPtr=NULL;
    q.size=0;
@@ -17,15 +22,27 @@ int main(int argc , char **argv) {
    int i,x;
    
 
- for(i=1;i<argc;i++){
+ /* for(i=1;i<argc;i++){
         if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
+            x = dequeue(&headPtr,&tailPtr);
             printf("dequeing %d\n",x);
         }
         else {
            enqueue(&headPtr,&tailPtr, atoi(argv[i]));
         }
  }
+  return 0; */
+  for(i=1;i<argc;i++){
+        if(strcmp(argv[i],"x")==0){
+            x = dequeue_struct(&q);
+            printf("dequeing %d\n",x);
+            q.size++; 
+        }
+        else {
+           enqueue_struct(&q , atoi(argv[i]));
+           q.size--; 
+        }
+ }
   return 0;
-  //54 x 8 3 x
+  //54 x 8 3 x x 68 95 x 
 }
--- Queue.h
+++ Queue.h
@@ -1,27 +1,38 @@
-
+#include "Node.h"
 
 typedef struct {
-	 NodePtr headPtr,tailPtr;
+	NodePtr headPtr,tailPtr;
 	int size;
 }Queue;
 
 
 void enqueue_struct(Queue* q, int x){
-  Node *new_node=(Node*) malloc(sizeof(Node));
-if(new_node){ 
-  /*Finish enqueue */
- }
+  Node *new_node=(Node*)malloc(sizeof(Node));
+  if(new_node){ 
+     new_node->data = x; 
+     new_node->nextPtr = NULL; 
+     if(q->headPtr == NULL) q->headPtr = new_node;  
+     else (q->tailPtr)->nextPtr = new_node; 
+     q->tailPtr = new_node; 
+     q->size++;
+    //Finish enqueue 
+  }
 }
 
 
 int dequeue_struct(Queue *q){
    NodePtr t=q->headPtr;
-   if(t){
+   if(t) {
    int value= t->data;
-       /*Finish dequeue */
+   q->headPtr = t->nextPtr; 
+   if(q->headPtr == NULL) q->tailPtr = NULL; 
+   free(t); 
+   q->size--; 
    return value;
+   //Finish Dequeue
    }
-   printf("Empty queue");
+   else { 
+   printf("Empty queue"); } 
    return 0;
 }
 
